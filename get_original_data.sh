af751a84b7320885ecbb7a406e884bd7807308e0
MunyDev <jeffplays1292@gmail.com>
2024-08-09 11:02:50 -0400

    thanks shell warnings

--- get_original_data.sh
+++ get_original_data.sh
@@ -4,12 +4,14 @@ echo This script may be removed or disabled with discretion of google.
 # Search directory for versions
 HIGHESTVERSIONAPPARENTLY=$(find  $HOME/.config/google-chrome/PKIMetadata/ -maxdepth 1 -mindepth 1 -type d| head -n 1)
 
-# Prepare output directory (version is 2000 for now. Don't ask why)
+#Let the user know what version we are on
+echo "$HIGHESTVERSIONAPPARENTLY"
 
+# Prepare output directory (version is 2000 for now. Don't ask why)
 mkdir -p original/PKIMetadata/2000
 
 #Copy latest version as base (idk if this is reliable)
-cp -rvf $HIGHESTVERSIONAPPARENTLY/. original/PKIMetadata/2000
+cp -rvf "$HIGHESTVERSIONAPPARENTLY"/. original/PKIMetadata/2000
 
 #Remove metadata and fingerprint(just sha256 of manifest) to be accepted
 rm -rvf original/PKIMetadata/2000/_metadata
