9f5f01fd9b1aeb1ff12df7db51465f29d861066a
Archimax <64978924+EnterTheVoid-x86@users.noreply.github.com>
2025-01-25 22:33:03 -0500

    Revert "fail statements + error handling"
    This reverts commit 5d167de02f030abea969a6e73066929049b6d574.

--- inshim.sh
+++ inshim.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-[ "$EUID" -ne 0 ] && fail "Not running as root, this shouldn't happen! Failing."
-
-fail() {
-	printf "%b\n" "$*" >&2 || :
-	sleep 1d
-}
+[ "$EUID" -ne 0 ] && echo "Run this as root"
 
 get_largest_cros_blockdev() {
 	local largest size dev_name tmp_size remo
@@ -36,15 +31,15 @@ format_part_number() {
 mount /dev/disk/by-label/STATE /mnt/stateful_partition/
 cros_dev="$(get_largest_cros_blockdev)"
 if [ -z "$cros_dev" ]; then
-    echo "No CrOS SSD found on device. Failing."
-    sleep 1d
+	echo "No CrOS SSD found on device. Failing."
+    exit 1
 fi
 stateful=$(format_part_number "$cros_dev" 1)
-mkfs.ext4 -F "$stateful" || fail "Failed to wipe stateful." # This only wipes the stateful partition 
-mount "$stateful" /tmp || fail "Failed to mount stateful."
+mkfs.ext4 -F "$stateful" # This only wipes the stateful partition
+mount "$stateful" /tmp
 mkdir -p /tmp/unencrypted
 cp /mnt/stateful_partition/usr/share/packeddata/. /tmp/unencrypted/ -rvf
 chown 1000 /tmp/unencrypted/PKIMetadata -R
 rm /tmp/.developer_mode
 umount /tmp
-crossystem disable_dev_request=1 || fail "Failed to set disable_dev_request."
+crossystem disable_dev_request=1
